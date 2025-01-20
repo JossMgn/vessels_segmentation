72a54d932985c293664bb79f1004acb80b2415a5
jossua.mignon.1@ulaval.ca <jossua.mgn@gmail.com>
2025-01-20 14:58:32 +0100

    Add a function to convert colored image into gray scale image.

--- mask.py
+++ mask.py
@@ -1,1 +1,22 @@
-print('hello')
+from utils import *
+from skimage import io
+import numpy as np
+
+def toGray(im, channel):
+    '''
+    Convert a colored image into a gray scale image.
+    -im: the image to convert
+    -channel: the method to convert:    "r" -> take only the red channel
+                                        "g" -> take only the green channel
+                                        "b" -> take only the blue channel
+                                        "mean" -> do the mean of the three channels
+    RETURN
+    -grayim: the one channel image
+    '''
+    channel_colors = ["r","g","b"]
+    if channel in channel_colors:
+        grayim = im[:,:,channel_colors.index(channel)]
+    elif channel == "mean":
+        grayim = np.mean(im, axis=2)
+    
+    return grayim
---
+++ utils.py
@@ -0,0 +1,45 @@
+from skimage import io
+from matplotlib import pyplot as plt
+import os
+from random import randint
+
+def showImg(image, imname=None):
+    '''
+        Allow to display image with a legend or not.
+        -image: the image that you want to display
+        -imname: the name of the image in the db
+    '''
+    io.imshow(image)
+    if imname is not None:
+        plt.title(imname)
+    plt.show()
+
+def readImg(path, lim=-1, rand=True):
+    '''
+        Read image(s) and display it at the end. You could chose to display all the db or a limited number of images.
+        -path: the path of the directory with images
+        -lim: the number max of images to display (-1 for all images, else chose a number)
+        -rand: allow to choose random images from the db or not (if not, the first images will be display)
+        RETURN
+        -a list of red images as ndarray(idx 0) and their name (idx 1)
+    '''
+    filenames = os.listdir(path)
+
+    if lim > 0 and lim < len(filenames):
+        if rand == True:
+            indexes = []
+            for i in range(lim):
+                new_idx = randint(0, len(filenames)-1)
+                while new_idx in indexes:
+                    new_idx = randint(0, len(filenames)-1)
+                indexes.append(new_idx)
+            filenames = [filenames[i] for i in indexes]
+        else:
+            filenames = filenames[0:lim]
+
+    filepaths = [os.path.join(path, name) for name in filenames]
+    images = []
+    for fp, fn in zip(filepaths, filenames):
+        images.append([io.imread(fp), fn])
+
+    return images
