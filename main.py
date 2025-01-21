97afc0ef27d7881b393784d054e2a753ae2cf4e7
jossua.mignon.1@ulaval.ca <jossua.mgn@gmail.com>
2025-01-21 14:31:10 +0100

    Design and development of the imToMask function which allow to retrieve the mask of the ROI of the image

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from utils import *
 from mask import *
+import time
 
 if __name__ == "__main__":
     filename = "./Data/InputData/training/images/21_training.tif"
     path = "./Data/InputData/training/images"
 
+    start = time.time()
     images = readImg(path, lim=3)
-    showImg(images[0][0], images[0][1])
+    end = time.time()
+    print(f"time for reading images: {round(end-start, 3)}sec.")
+    # showImg(images[0][0], images[0][1])
 
-    for ch in ["r","g","b","mean"]:
-        grayim = toGray(images[0][0], channel=ch)
-        showImg(grayim, images[0][1])
+    # for ch in ["r","g","b","mean"]:
+    #     grayim = toGray(images[0][0], channel=ch)
+    #     showImg(grayim, images[0][1])
+    start = time.time()
+    grayim = toGray(images[0][0], channel="g")
+    mask = imToMask(grayim)
+    end = time.time()
+    print(f"time to create the mask from the colored image: {round(end-start, 3)}sec.")
+
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].imshow(images[0][0])
+    axs[0, 0].set_title(images[0][1])
+    axs[0, 1].imshow(grayim, cmap='gray')
+    axs[0, 1].set_title('Image in grayscale ch=g')
+    axs[1, 0].imshow(mask)
+    axs[1, 0].set_title('mask')
+    for a in axs.ravel():
+        a.axis('off')
+    plt.tight_layout()
+    plt.show()
+
--- mask.py
+++ mask.py
@@ -1,15 +1,18 @@
 from utils import *
-from skimage import io
+from skimage import io, draw
+from skimage import morphology as mrph
+from skimage import filters as flt
 import numpy as np
+from example import *
 
 def toGray(im, channel):
     '''
     Convert a colored image into a gray scale image.
     -im: the image to convert
-    -channel: the method to convert:    "r" -> take only the red channel
-                                        "g" -> take only the green channel
-                                        "b" -> take only the blue channel
-                                        "mean" -> do the mean of the three channels
+    -channel: the method to convert:    \n\t"r" -> take only the red channel
+                                        \n\t"g" -> take only the green channel
+                                        \n\t"b" -> take only the blue channel
+                                        \n\t"mean" -> do the mean of the three channels
     RETURN
     -grayim: the one channel image
     '''
@@ -18,5 +21,43 @@ def toGray(im, channel):
         grayim = im[:,:,channel_colors.index(channel)]
     elif channel == "mean":
         grayim = np.mean(im, axis=2)
-    
+
+    grayim = (grayim/255).astype(np.float32)
     return grayim
+
+def imToMask(im):
+    '''
+    -im: gray image use to create the mask.\n
+    RETURN
+    -mask: the mask of the im in output.
+    '''
+    edges = flt.sobel(im)
+
+    low = 0.01
+    high = 0.1
+
+    hyst = flt.apply_hysteresis_threshold(edges, low, high) #détection des bords par hysteresis
+
+    x, y = np.shape(im)
+    seed = np.zeros((x, y), dtype=np.float32)
+    seed[0:50,0:50] = 0.5 # Création d'une image de marqueur pour reconstruire l'extérieur
+
+    mask = mrph.reconstruction(seed, 1-hyst, 'dilation') #Utilisation de la dilatation géodésique pour reconstruire les bords extérieurs
+    
+    # fig, ax = plt.subplots(nrows=2, ncols=2)
+    # ax[0, 0].imshow(im, cmap='gray')
+    # ax[0, 0].set_title('Original image')
+    # ax[0, 1].imshow(edges, cmap='magma')
+    # ax[0, 1].set_title('Sobel edges')
+    # ax[1, 0].imshow(hyst, cmap='magma')
+    # ax[1, 0].set_title('hysteresis')
+    # ax[1, 1].imshow(mask, cmap='magma')
+    # ax[1, 1].set_title('mask')
+    # for a in ax.ravel():
+    #     a.axis('off')
+    # plt.tight_layout()
+    # plt.show()
+
+    return 1-mask
+
+
